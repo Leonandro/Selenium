72a7794be9684e03fc5aed46da9c767368a36e67
Leonandro <leonandrogurgel@gmail.com>
2022-01-03 04:08:12 -0300

    Adjusting indentation and adding comments

--- NotionLoginTest.java
+++ NotionLoginTest.java
@@ -4,10 +4,8 @@ import static org.junit.Assert.fail;
 
 import java.time.Duration;
 
-
 import org.junit.Test;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.By;
 
@@ -19,156 +17,159 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NotionLoginTest {
-		
-		private Constants resources;
-		private WebDriver driver;
-		
 
-		@Before
-		public void setUp() {
-			resources = new Constants ();
-			System.setProperty("webdriver.chrome.driver", this.resources.DRIVER_PATH);
-			driver = new ChromeDriver();
-			driver.get(this.resources.LOGIN_URL);
+	private Constants resources;
+	private WebDriver driver;
+
+	@Before
+	public void setUp() {
+		
+		resources = new Constants();
+		
+		//Setting the driver using a local driver for chrome
+		System.setProperty("webdriver.chrome.driver", this.resources.DRIVER_PATH);
+		
+		driver = new ChromeDriver();
+		
+		//Opening the login URL page
+		driver.get(this.resources.LOGIN_URL);
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void testLoginWithInvalidEmail() throws InterruptedException {
+
+		// Finding the input for email by the id
+		WebElement emailInput = driver.findElement(By.id("notion-email-input-1"));
+
+		// Writing the invalid email
+		emailInput.sendKeys("invalidEmail");
+
+		// Submitting the invalid email
+		emailInput.sendKeys(Keys.ENTER);
+
+		Thread.sleep(this.resources.NORMAL_WAIT_TIME);
+
+		// Is expected that the input for password won't appear
+		try {
+			driver.findElement(By.id("notion-password-input-2"));
+			fail("A invalid email pass on the validation, this is bad!");
+		} catch (Exception e) {
 		}
-	    @After
-	    public void tearDown() {
-	      driver.quit();
-	    }
 
-	    @Test
-		public void testLoginWithInvalidEmail() throws InterruptedException {
+	}
 
-			
-	    	//Finding the input for email by the id
-	        WebElement emailInput = driver.findElement(By.id("notion-email-input-1"));
-	        
-	        //Writing the invalid email
-	        emailInput.sendKeys("invalidEmail");
-	        	
-	        //Submitting the invalid email
-	        emailInput.sendKeys(Keys.ENTER);
-	        
-	        Thread.sleep(this.resources.NORMAL_WAIT_TIME);
-	        
-	        //Is expected that the input for password won't appear
-	        try {
-	        	driver.findElement(By.id("notion-password-input-2"));
-	        	fail("A invalid email pass on the validation, this is bad!");
-	        } catch (Exception e) {
-	        }
-	        
-	      
-	    }
-		
-		@Test
-		public void testLoginWithCorrectEmailAndWrongPWD() throws InterruptedException {
-		
-			
-	    	//Finding the input for email by the id
-	        WebElement emailInput = driver.findElement(By.id("notion-email-input-1"));
-	        
-	        //Writing the valid email 
-	        emailInput.sendKeys(this.resources.EMAIL);
-	        	
-	        //Submitting the valid email
-	        emailInput.sendKeys(Keys.ENTER);
-	        
-	        //Waiting until the password input appear
-	        WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(4))
-	                .until(ExpectedConditions.elementToBeClickable(By.id("notion-password-input-2")));
-	        
-	    	//Finding the input for password by the id
-	        WebElement passwordInput = driver.findElement(By.id("notion-password-input-2"));
-	        
-	        //Writing the invalid password
-	        passwordInput.sendKeys("invalidPassword");
-	        
-	        //Submitting the invalid password
-	        passwordInput.sendKeys(Keys.ENTER);
-	       	        
-	        Thread.sleep(this.resources.NORMAL_WAIT_TIME);
-	       
-	        //Is expected that the page will not be redirected to the home page
-	        assertEquals(driver.getCurrentUrl(), this.resources.LOGIN_URL);
-	    }
-		
-		@Test
-		public void testLoginWithCorrectEmailAndCorrectPWD() throws InterruptedException {
-		
-	    	//Finding the input for email by the id
-	        WebElement emailInput = driver.findElement(By.id("notion-email-input-1"));
-	        
-	        //Writing the valid email 
-	        emailInput.sendKeys(this.resources.EMAIL);
-	        	
-	        //Submitting the valid email
-	        emailInput.sendKeys(Keys.ENTER);
-	        
-	        //Waiting until the password input appear
-	        WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(4))
-	                .until(ExpectedConditions.elementToBeClickable(By.id("notion-password-input-2")));
-	        
-	    	//Finding the input for password by the id
-	        WebElement passwordInput = driver.findElement(By.id("notion-password-input-2"));
-	        
-	        //Writing the valid password
-	        passwordInput.sendKeys(this.resources.PASSWORD);
-	        
-	        //Submitting the valid password
-	        passwordInput.sendKeys(Keys.ENTER);
-	       	        
-	        Thread.sleep(this.resources.LOGIN_WAIT_TIME);
-	        
-	        //Is expected that the page will be redirected to the home page
-	        assertNotEquals(driver.getCurrentUrl(), this.resources.LOGIN_URL);
-	    }
-		
-		@Test
-		public void testLogout() throws InterruptedException {
-		
-	    	//Finding the input for email by the id
-	        WebElement emailInput = driver.findElement(By.id("notion-email-input-1"));
-	        
-	        //Writing the valid email 
-	        emailInput.sendKeys(this.resources.EMAIL);
-	        	
-	        //Submitting the valid email
-	        emailInput.sendKeys(Keys.ENTER);
-	        
-	        //Waiting until the password input appear
-	        WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(4))
-	                .until(ExpectedConditions.elementToBeClickable(By.id("notion-password-input-2")));
-	        
-	    	//Finding the input for password by the id
-	        WebElement passwordInput = driver.findElement(By.id("notion-password-input-2"));
-	        
-	        //Writing the valid password
-	        passwordInput.sendKeys(this.resources.PASSWORD);
-	        
-	        //Submitting the valid password
-	        passwordInput.sendKeys(Keys.ENTER);
-	       	        
-	        Thread.sleep(this.resources.LOGIN_WAIT_TIME);
-	        
-	    	//Finding the account options button by the xpath
-	        WebElement options = driver.findElement(By.xpath("//*[text()=\"leonandro's Notion\"]"));
-	        
-	        //Opening the account options
-		    options.click();
-		    
-	        Thread.sleep(this.resources.CLICK_WAIT_TIME);
-	        
-	    	//Finding the account logout button by the xpath
-	        WebElement logout = driver.findElement(By.xpath("//*[text()='Log out all']"));
-	        
-	        //Logging out
-		    logout.click();
-		    
-	        Thread.sleep(this.resources.LOGIN_WAIT_TIME);
-	        
-	        //Is expected that the page will be redirected to the base url of the site
-	        assertEquals(driver.getCurrentUrl(), this.resources.BASE_URL);
-	    }
+	@Test
+	public void testLoginWithCorrectEmailAndWrongPWD() throws InterruptedException {
+
+		// Finding the input for email by the id
+		WebElement emailInput = driver.findElement(By.id("notion-email-input-1"));
+
+		// Writing the valid email
+		emailInput.sendKeys(this.resources.EMAIL);
+
+		// Submitting the valid email
+		emailInput.sendKeys(Keys.ENTER);
+
+		// Waiting until the password input appear
+		WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(4))
+				.until(ExpectedConditions.elementToBeClickable(By.id("notion-password-input-2")));
+
+		// Finding the input for password by the id
+		WebElement passwordInput = driver.findElement(By.id("notion-password-input-2"));
+
+		// Writing the invalid password
+		passwordInput.sendKeys("invalidPassword");
+
+		// Submitting the invalid password
+		passwordInput.sendKeys(Keys.ENTER);
+
+		Thread.sleep(this.resources.NORMAL_WAIT_TIME);
+
+		// Is expected that the page will not be redirected to the home page
+		assertEquals(driver.getCurrentUrl(), this.resources.LOGIN_URL);
+	}
+
+	@Test
+	public void testLoginWithCorrectEmailAndCorrectPWD() throws InterruptedException {
+
+		// Finding the input for email by the id
+		WebElement emailInput = driver.findElement(By.id("notion-email-input-1"));
+
+		// Writing the valid email
+		emailInput.sendKeys(this.resources.EMAIL);
+
+		// Submitting the valid email
+		emailInput.sendKeys(Keys.ENTER);
+
+		// Waiting until the password input appear
+		WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(4))
+				.until(ExpectedConditions.elementToBeClickable(By.id("notion-password-input-2")));
+
+		// Finding the input for password by the id
+		WebElement passwordInput = driver.findElement(By.id("notion-password-input-2"));
+
+		// Writing the valid password
+		passwordInput.sendKeys(this.resources.PASSWORD);
+
+		// Submitting the valid password
+		passwordInput.sendKeys(Keys.ENTER);
+
+		Thread.sleep(this.resources.LOGIN_WAIT_TIME);
+
+		// Is expected that the page will be redirected to the home page
+		assertNotEquals(driver.getCurrentUrl(), this.resources.LOGIN_URL);
+	}
+
+	@Test
+	public void testLogout() throws InterruptedException {
+
+		// Finding the input for email by the id
+		WebElement emailInput = driver.findElement(By.id("notion-email-input-1"));
+
+		// Writing the valid email
+		emailInput.sendKeys(this.resources.EMAIL);
+
+		// Submitting the valid email
+		emailInput.sendKeys(Keys.ENTER);
+
+		// Waiting until the password input appear
+		WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(4))
+				.until(ExpectedConditions.elementToBeClickable(By.id("notion-password-input-2")));
+
+		// Finding the input for password by the id
+		WebElement passwordInput = driver.findElement(By.id("notion-password-input-2"));
+
+		// Writing the valid password
+		passwordInput.sendKeys(this.resources.PASSWORD);
+
+		// Submitting the valid password
+		passwordInput.sendKeys(Keys.ENTER);
+
+		Thread.sleep(this.resources.LOGIN_WAIT_TIME);
+
+		// Finding the account options button by the xpath
+		WebElement options = driver.findElement(By.xpath("//*[text()=\"leonandro's Notion\"]"));
+
+		// Opening the account options
+		options.click();
+
+		Thread.sleep(this.resources.CLICK_WAIT_TIME);
+
+		// Finding the account logout button by the xpath
+		WebElement logout = driver.findElement(By.xpath("//*[text()='Log out all']"));
+
+		// Logging out
+		logout.click();
+
+		Thread.sleep(this.resources.LOGIN_WAIT_TIME);
+
+		// Is expected that the page will be redirected to the base url of the site
+		assertEquals(driver.getCurrentUrl(), this.resources.BASE_URL);
+	}
 
 }
